586d92783f59fcd1421e866c8a043aa37c98471c
minhlag99 <66405955+minhlag99@users.noreply.github.com>
2020-11-06 01:54:42 +0700

    Add project files.

---
+++ TMDT_Tuan4/Controllers/LaptopController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TMDT_Tuan4.Models;
+namespace TMDT_Tuan4.Controllers
+{
+    public class LaptopController : Controller
+    {
+        TMDT5Entities db = new TMDT5Entities();
+        // GET: Laptop
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult getCompany()
+        {
+            var ListCompany = db.laptops.GroupBy(l => l.Company).Select(l => l.Key);
+            return PartialView(ListCompany);
+        }
+
+        public ActionResult getCPU()
+        {
+            var ListCPU = db.laptops.GroupBy(l => l.Cpu).Select(l => l.Key);
+            return PartialView(ListCPU);
+        }
+
+        public ActionResult getMemory()
+        {
+            var ListMemory = db.laptops.GroupBy(l => l.Memory).Select(l => l.Key);
+            return PartialView(ListMemory);
+        }
+
+        public ActionResult getScreenResolution()
+        {
+            var ListScreenResolution = db.laptops.GroupBy(l => l.ScreenResolution).Select(l => l.Key);
+            return PartialView(ListScreenResolution);
+        }
+
+        public ActionResult getTypeName()
+        {
+            var ListTypeName = db.laptops.GroupBy(l => l.TypeName).Select(l => l.Key);
+            return PartialView(ListTypeName);
+        }
+
+        public ActionResult getGPU()
+        {
+            var ListGPU = db.laptops.GroupBy(l => l.Gpu).Select(l => l.Key);
+            return PartialView(ListGPU);
+        }
+
+        public ActionResult getOS()
+        {
+            var ListOS = db.laptops.GroupBy(l => l.OpSys).Select(l => l.Key);
+            return PartialView(ListOS);
+        }
+
+        public ActionResult getInches()
+        {
+            var ListInches = db.laptops.GroupBy(l => l.Inches).Select(l => l.Key);
+            return PartialView(ListInches);
+        }
+
+        public ActionResult getSpeed()
+        {
+            var ListSpeed = db.laptops.GroupBy(l => l.Speed).Select(l => l.Key);
+            return PartialView(ListSpeed);
+        }
+
+        public ActionResult getPriceLow()
+        {
+            var ListPriceLow = db.laptops.GroupBy(l => l.Price_euros).Select(l => l.Key);
+            return PartialView(ListPriceLow);
+        }
+
+        public ActionResult getWeight()
+        {
+            var ListWeight = db.laptops.GroupBy(l => l.Weight).Select(l => l.Key);
+            return PartialView(ListWeight);
+        }
+
+        public ActionResult getRam()
+        {
+            var ListRam = db.laptops.GroupBy(l => l.Ram).Select(l => l.Key);
+            return PartialView(ListRam);
+        }
+    }
+}
---
+++ TMDT_Tuan4/Helper/SortCriteria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+namespace TMDT_Tuan4.Helper
+{
+    public enum SortCriteria
+    {
+        [Description("Relevance")]
+        Relevance = 0,
+        [Description("Price: Low to High")]
+        PriceLowToHigh = 1,
+        [Description("Price: High to Low")]
+        PriceHighToLow = 2
+    }
+}
